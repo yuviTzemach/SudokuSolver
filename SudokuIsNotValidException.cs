4c8e92cdae9fd80fe7a9ce6fd52af71822d22b18
Yuval <yuvaltze2004@gmail.com>
2023-01-01 21:47:49 +0200

    creating new exception

---
+++ SudokuIsNotValidException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject2
+{
+    internal class SudokuIsNotValidException : Exception
+    {
+        public SudokuIsNotValidException() { }
+
+        public SudokuIsNotValidException(string message) : base(message) { }
+
+        public SudokuIsNotValidException(string message, Exception inner) : base(message, inner) { }
+    }
+}
