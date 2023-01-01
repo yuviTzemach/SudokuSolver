7e08d8e0a11c348407918a3ef9b366848fe55666
Yuval <yuvaltze2004@gmail.com>
2023-01-01 22:25:43 +0200

    using the menu in the main program

---
+++ SudokuSolver.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalProject2.Sudoku;
+
+namespace FinalProject2
+{
+    internal class SudokuSolver
+    {
+        public static void Main(String[] args)
+        {
+            //calling the menu
+            Menu.menu();
+        }
+    }
+}
